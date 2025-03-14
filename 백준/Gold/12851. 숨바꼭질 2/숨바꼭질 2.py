aedf1873fdb4a758ea4bc639ece7d0fb6f30c4ca
SSUM <116950962+ssum21@users.noreply.github.com>
2025-03-14 20:33:33 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 160 ms, Memory: 38072 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, K = map(int, input().split())
+
+queue = deque([n])
+dist = [-1 for _ in range(100001)]
+ways = [0 for _ in range(100001)]
+
+dist[n] = 0
+ways[n] = 1
+
+while queue:
+    k = queue.popleft()
+    for i in (k-1, k+1, k*2):
+        if 0 <= i < 100001:
+            if dist[i] == -1:
+                dist[i] = dist[k] + 1
+                ways[i] = ways[k]
+                queue.append(i)
+            elif dist[i] == dist[k] + 1:
+                ways[i] += ways[k]
+
+print(dist[K])
+print(ways[K])
