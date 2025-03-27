7aa5c3d013fafcd06aca0123f11892ea53788dcd
SSUM <116950962+ssum21@users.noreply.github.com>
2025-03-28 00:23:39 +0900

    [Bronze II] Title: 경로당펑크 2077, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25205. 경로당펑크 2077/경로당펑크 2077.py
@@ -0,0 +1,7 @@
+
+N=int(input())
+s=input()
+if s[-1]=='k' or s[-1] == 'o' or s[-1] =='i' or s[-1] =='O'or s[-1] =='j'or s[-1] =='p'or s[-1] =='u'or s[-1] =='P'or s[-1] =='h'or s[-1] =='y'or s[-1] =='n'or s[-1] =='b'or s[-1] =='m'or s[-1] =='l':
+    print(0)
+else:
+    print(1)
